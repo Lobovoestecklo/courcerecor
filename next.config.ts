ef4a36b5bce0479b1e889a39e243b8d60b38c92e
Daria <d.trochinskaya@gmail.com>
2025-08-07 12:30:24 +0100

    Remove webpack configuration to fix __dirname issue in Edge Runtime

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 import type { NextConfig } from 'next';
-import CopyPlugin from 'copy-webpack-plugin';
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const nextConfig: NextConfig = {
   experimental: {
@@ -16,26 +10,6 @@ const nextConfig: NextConfig = {
         hostname: 'avatar.vercel.sh',
       },
     ],
-  },
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    config.plugins = config.plugins || [];
-
-    config.plugins.push(
-      new CopyPlugin({
-        patterns: [
-          {
-            from: path.join(
-              path.resolve(
-                __dirname,
-                'node_modules/pdfjs-dist/build/pdf.worker.min.mjs',
-              ),
-            ),
-            to: path.join(path.resolve(__dirname, 'public')),
-          },
-        ],
-      }),
-    );
-    return config;
   },
 };
 
